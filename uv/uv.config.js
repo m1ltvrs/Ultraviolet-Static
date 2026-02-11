2fdbbff9df638e76c1e6d6638a37ff3c7689b445
TheMltivrs <rturner260@lsr7.net>
2026-02-11 03:40:58 -0600

    Update bare URL in uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
-    prefix: '/service/',
-    bare: '/bare/',
+    prefix: '/service/',  
+    bare: 'https://bare.bare.run/',  
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
